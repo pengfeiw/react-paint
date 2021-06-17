ef2c045c9401889fd64678a8d0f2cdd46045f830
wangpengfei <2601339907@qq.com>
2021-06-17 13:27:03 +0800

    chore: 隐藏放大和文字功能

--- src/components/toolBar/tool/index.tsx
+++ src/components/toolBar/tool/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import CreateTwoTone from "@material-ui/icons/CreateTwoTone";
 import FormatColorFillTwoTone from "@material-ui/icons/FormatColorFillTwoTone";
-import TextFieldsIcon from "@material-ui/icons/TextFields";
 import ColorizeTwoToneIcon from "@material-ui/icons/ColorizeTwoTone";
-import SearchTwoToneIcon from "@material-ui/icons/SearchTwoTone";
+// import SearchTwoToneIcon from "@material-ui/icons/SearchTwoTone";
+// import TextFieldsIcon from "@material-ui/icons/TextFields";
 import {ToolType} from "../../../util/toolType";
 import {ToolTypeContext} from "../../../context";
 import "./index.less";
@@ -25,23 +25,23 @@ const ToolPanel: React.FC<ToolPanelProps> = (props) => {
                                 <span title="铅笔">
                                     <CreateTwoTone className={type === ToolType.PEN ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.PEN)}} />
                                 </span>
+                                <span title="橡皮擦">
+                                    <img src="./icon/eraser.svg" className={type === ToolType.ERASER ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.ERASER)}} />
+                                </span>
                                 <span title="填充">
                                     <FormatColorFillTwoTone className={type === ToolType.COLOR_FILL ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.COLOR_FILL)}} />
-                                </span>
-                                <span title="文字">
-                                    <TextFieldsIcon className={type === ToolType.TEXT ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.TEXT)}} />
                                 </span>
                             </div>
                             <div className="down">
                                 <span title="颜色选取器">
                                     <ColorizeTwoToneIcon className={type === ToolType.COLOR_EXTRACT ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.COLOR_EXTRACT)}} />
                                 </span>
-                                <span title="橡皮擦">
-                                    <img src="./icon/eraser.svg" className={type === ToolType.ERASER ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.ERASER)}} />
+                                {/* <span title="文字">
+                                    <TextFieldsIcon className={type === ToolType.TEXT ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.TEXT)}} />
                                 </span>
                                 <span title="放大镜">
                                     <SearchTwoToneIcon className={type === ToolType.MAGNIFYING ? `tool-item ${selectedToolClass}` : "tool-item"} onClick={() => {setType(ToolType.MAGNIFYING)}} />
-                                </span>
+                                </span> */}
                             </div>
                         </>
                     )
